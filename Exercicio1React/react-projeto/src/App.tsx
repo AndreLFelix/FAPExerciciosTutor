e54bb8c937dc46605234e2a74088ebe6d56086a7
Andre Luiz <144123789+AndreLFelix@users.noreply.github.com>
2024-01-25 11:58:29 -0300

    Add files via upload

---
+++ Exercicio1React/react-projeto/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Board from './componentes/Board';
+
+function App() {
+  
+  return (
+    <div>
+      <h1>Aplicativo React de alfabeto</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
